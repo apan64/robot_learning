ae00f010e6c999c414fd1d154c22a06ec3c600b7
robertsiegel <rsiegel812@gmail.com>
2018-10-29 11:42:01 -0400

    Data collection script runnable

--- data_processing_utilities/scripts/data_collection.py
+++ data_processing_utilities/scripts/data_collection.py
@@ -7,6 +7,7 @@ import rospy
 from geometry_msgs.msg import Twist, Vector3
 
 from nav_msgs.msg import Odometry
+import tf.transformations as t
 
 import subprocess
 import time
@@ -17,23 +18,26 @@ class DataGenerator(object):
         rospy.init_node("data_generator")    
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.angle = 0
-        self.rotate_speed = .5 
-        self.rotate_time = 1 # amount of time rotating between recording, used in rotate function
+        self.rotate_speed = .25 
+        self.rotate_time = .5 # amount of time rotating between recording, used in rotate function
 
     def rotate(self):
         self.vel_pub.publish(Twist(Vector3(0, 0, 0), Vector3(0, 0, -1 * self.rotate_speed)))
         time.sleep(self.rotate_time)
-        self.vel_pub(Twist(Vector3(0, 0, 0), Vector3(0, 0, 0)))
+        self.vel_pub.publish(Twist(Vector3(0, 0, 0), Vector3(0, 0, 0)))
 
-    def odometry_callback(self):
+    def odometry_callback(self, msg):
         """ Update the angle of the neato """
         transformed_pos = self.convert_pose_to_xy_and_theta(msg.pose.pose) # transform to x,y,yaw
         self.angle = math.degrees(transformed_pos[2])
+        print ("radians: {}, degrees: {}".format(transformed_pos[2], self.angle))
 
     def executeBashFile(self):
         """ Execute the bash file that will record audio"""
-        bashCommand = "../data/collect.sh {}".format(self.angle)
-        output = subprocess.check_output(['bash','-c', bashCommand])
+        bash_command = "../data/collect.sh"
+        angle_string = str(self.angle)
+        
+        subprocess.call([bash_command, angle_string])
 
     def convert_pose_to_xy_and_theta(self, pose):
         """ Convert pose (geometry_msgs.Pose) to a (x,y,yaw) tuple """
