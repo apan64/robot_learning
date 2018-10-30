0d0c49b756de4422f16b19d554adf1693cc54b1b
apan64 <andrew.pan@students.olin.edu>
2018-10-30 14:21:27 -0400

    changed script being remote called

--- data_processing_utilities/data/collect.sh
+++ data_processing_utilities/data/collect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 a=$1 
-sshpass -p "raspberry" ssh pi@192.168.17.201 "~pi/use_case_scripts/ride-of-the-neatos/audio_stream.sh -q" > recording_${a}.wav
+sshpass -p "raspberry" ssh pi@192.168.17.201 "~pi/use_case_scripts/ride-of-the-neatos/audio_stream_timed.sh -q" > recording_${a}.wav
 
